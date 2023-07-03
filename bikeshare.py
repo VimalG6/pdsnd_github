7e624e2aa0c6351d0e4b6c5af71190a309ff82ac
Vimal Govind <vimal.govind@angloamerican.com>
2023-07-03 15:40:21 +0200

    Changed welcome message

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data today !')
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
@@ -164,6 +164,7 @@ def trip_duration_stats(df):
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
+
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
